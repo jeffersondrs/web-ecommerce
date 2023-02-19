be98d5e1cc80f093d1a3266ec41d8be452946557
jeffersondrs <jeffersonduarte@id.uff.br>
2023-02-19 15:26:42 -0300

    router dom fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
@@ -11,4 +10,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </HashRouter>
   </React.StrictMode>
 );
-
