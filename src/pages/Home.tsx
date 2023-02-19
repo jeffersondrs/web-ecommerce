8b1a59c68c98717bedab3d99a9000191f09d4355
jeffersondrs <jeffersonduarte@id.uff.br>
2023-02-19 12:31:11 -0300

    products page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./pages.styles.scss";
-import HomeContent from "../components/home-content/Home-conten.component";
+import HomeContent from "../components/home-content/Home-content.component";
 export default function Home() {
   return (
     <div className="home">
